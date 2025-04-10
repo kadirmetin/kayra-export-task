c906314a208c28fa24646d41a7ebad47f492e5da
Kadir Metin <kadir-metin@outlook.com.tr>
2025-04-10 23:23:10 +0300

    initialize Next.js project with basic configuration

---
+++ products-remote/pages/index.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      PRODUCTS REMOTE
+    </div>
+  );
+}
