bc1f2be6c843fee2ebc1060620595d27e526e5d3
Kadir Metin <kadir-metin@outlook.com.tr>
2025-04-10 22:22:06 +0300

    initialize Next.js project with basic configuration

---
+++ host/pages/index.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      HOST
+    </div>
+  );
+}
