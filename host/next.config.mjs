abb51611817be14ba37cee1deba55052d938eb1a
Kadir Metin <kadir-metin@outlook.com.tr>
2025-04-10 22:27:35 +0300

    Ant Design and SF Pro Text font integration

--- host/next.config.mjs
+++ host/next.config.mjs
@@ -3,6 +3,18 @@
 const nextConfig = {
   /* config options here */
   reactStrictMode: true,
+
+  transpilePackages: [
+    "antd",
+    "@ant-design",
+    "rc-util",
+    "rc-pagination",
+    "rc-picker",
+    "rc-notification",
+    "rc-tooltip",
+    "rc-tree",
+    "rc-table",
+  ],
 };
 
 export default nextConfig;
--- host/pages/_app.tsx
+++ host/pages/_app.tsx
@@ -1,6 +1,22 @@
+import { ConfigProvider, Layout } from "antd";
+import { Content } from "antd/es/layout/layout";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          fontFamily: "SF Pro Text",
+        },
+      }}
+    >
+      <Layout>
+        <Content>
+          <Component {...pageProps} />
+        </Content>
+      </Layout>
+    </ConfigProvider>
+  );
 }
