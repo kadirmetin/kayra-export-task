e7b048340af46e9b84626786f25b64f8e61b71e7
Kadir Metin <kadir-metin@outlook.com.tr>
2025-04-12 17:09:32 +0300

    initialize basket-remote project with React, TypeScript, and Webpack

---
+++ basket-remote/src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div>BASKET REMOTE</div>;
+};
+
+export default App;
---
+++ basket-remote/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    path: path.join(__dirname, "/dist"), // the bundle output path
+    filename: "main.js", // the name of the bundle
+  },
+  resolve: {
+    modules: [__dirname, "src", "node_modules"],
+    extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html", // to import index.html file inside index.js
+    }),
+  ],
+  devServer: {
+    port: 3002,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
